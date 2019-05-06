48e48a97d7861c2c258f77eee7910994f59a8c9e
Dabbyd123 <dbrocker89@gmail.com>
2019-05-06 00:51:38 -0500

    I DID IT!!

--- src/components/Rovers.js
+++ src/components/Rovers.js
@@ -4,13 +4,13 @@ const Rovers = (props) => {
     return (
         <div className="rovers">
             <div type="button" onClick={props.clickHandler} id="Curiosity" className="rover Curiosity">
-                <h1>Curiosity</h1>
+                Curiosity
             </div>
             <div type="button" onClick={props.clickHandler} id="Opportunity" className="rover Opportunity">
-                <h1>Opportunity</h1>
+                Opportunity
             </div>
             <div type="button" onClick={props.clickHandler} id="Spirit" className="rover Spirit">
-                <h1>Spirit</h1>
+                Spirit
             </div>
         </div>
     )
